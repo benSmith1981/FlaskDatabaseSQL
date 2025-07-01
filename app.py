3c66571fd4f12b13f96fe2b911c8366f624ab5b2
Ben Smith <benjaminsmith1981@googlemail.com>
2025-07-01 16:27:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -141,6 +141,4 @@ def about():
 
 if __name__ == '__main__':
     print("__main__")
-    app.run(debug=True)
-
-    # app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
